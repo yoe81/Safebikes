96e10803e90eb8af382994011b267f93b0cf420f
yoe81 <yoe81@hotmail.com>
2017-11-23 12:51:58 -0500

    se agrega boton para registro y contador de visitas

--- js/script.js
+++ js/script.js
@@ -31,3 +31,48 @@ function mySlide(param){
                 document.getElementById( 'slide' ).src = images[i ];
             }
         }
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//coockie
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("Usuario");
+    if (user != "") {
+        alert("Hola de nuevo " + user);
+    } else {
+       user = prompt("Ingresa tu nombre de usuario:","");
+       if (user != "" && user != null) {
+           setCookie("usuario", user, 30);
+       }
+    }
+}
+
